40faf08b46e815ec5eac7227c18ea03b5ae41628
Belt0M <belongtomars@gmail.com>
2023-12-13 17:15:34 +0100

    Add levelElements collision  check

--- js/script.js
+++ js/script.js
@@ -26,12 +26,6 @@ class Game {
 
 		this.gameField.appendChild(this.app.view)
 
-		// Set canvas background image
-		// let img = PIXI.Sprite.from('/assets/img/background.png')
-		// img.width = 1024
-		// img.height = 768
-		// this.app.stage.addChild(img)
-
 		this.levelManager = new LevelManager()
 		this.level = this.levelManager.getLevel(0)
 
@@ -140,7 +134,7 @@ class Game {
 	handleMouseDown(event) {
 		const element = this.findSelectedElement(event)
 
-		if (element) {
+		if (element && element.type !== 'block') {
 			this.activeElement = element
 			this.activeElement.alpha = 0.75
 		}
@@ -170,39 +164,80 @@ class Game {
 			const isVerticalMovement = Math.abs(deltaY) > Math.abs(deltaX)
 
 			// Check collisions
-			if (this.checkCollision()) return
+			// if (this.checkCollision()) return
 
 			// Update the element's position based on the movement direction
+			const activeElPosition = this.activeElement.position
 			if (isHorizontalMovement) {
 				const sign = Math.sign(deltaX)
-				if (deltaX > sign * TILE_SIZE)
-					this.activeElement.position.x += sign * TILE_SIZE
+				console.log(deltaX, deltaX > sign * TILE_SIZE * 2, sign * TILE_SIZE)
+				if (deltaX > sign * TILE_SIZE) {
+					if (
+						this.checkCollision(
+							activeElPosition.x + sign * TILE_SIZE,
+							activeElPosition.y
+						)
+					) {
+						return
+					}
+
+					activeElPosition.x += sign * TILE_SIZE
+				}
 			} else if (isVerticalMovement) {
 				const sign = Math.sign(deltaY)
 				if (deltaY > sign * TILE_SIZE) {
-					this.activeElement.position.y += sign * TILE_SIZE
+					if (
+						this.checkCollision(
+							activeElPosition.x,
+							activeElPosition.y + sign * TILE_SIZE
+						)
+					) {
+						return
+					}
+
+					activeElPosition.y += sign * TILE_SIZE
 				}
 			}
 		}
 	}
 
-	checkCollision() {
+	checkCollision(x, y) {
 		// Check for collisions with board elements
-		for (const element of this.boardElements) {
-			if (
-				element.type === 0 &&
-				element.getBounds().contains(this.activeElement.position)
-			) {
-				return true
-			}
-		}
+		// for (const element of this.boardElements) {
+		// 	element.type === 1 &&
+		// 		console.log(
+		// 			element.x,
+		// 			element.y,
+		// 			x,
+		// 			y,
+		// 			!(element.x === x && element.y === y)
+		// 		)
+		// 	if (element.type === 1 && !(element.x === x && element.y === y)) {
+		// 		console.log('end')
+		// 		return true
+		// 	}
+		// }
 
 		// Check for collisions with level elements
 		for (const element of this.levelElements) {
+			// Create array with all level elements except active one
+			const elementsArray = ['block', 'water', 'fire', 'earth', 'air']
+			elementsArray.splice(elementsArray.indexOf(this.activeElement.type), 1)
+			elementsArray.includes(element.type) &&
+				console.log(
+					element.type,
+					element.x,
+					element.y,
+					x,
+					y,
+					element.x === x && element.y === y
+				)
 			if (
-				element.type === 'block' &&
-				element.getBounds().contains(this.activeElement.position)
+				elementsArray.includes(element.type) &&
+				element.x === x &&
+				element.y === y
 			) {
+				console.log('end')
 				return true
 			}
 		}
